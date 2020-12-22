151a88d027d22bf4f9b56b33c6ee848d9c58ee21
Christopher Chalcraft <chris.chalcraft@gmail.com>
2020-12-22 11:21:13 -0700

    Adding the main resource group creation

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "databricks-rg-cchalc-20201215"
+  location = "westus2"
+}
